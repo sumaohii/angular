12317bdfa1256cf09f24833b0ddecd467032e2fd
duonglongnhat88 <duonglongnhat88@gmail.com>
2019-06-25 08:59:44 +0700

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/Navbar/navbar.component';
+import { MaincontentComponent } from './components/Maincontent/maincontent.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MaincontentComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavbarComponent,
+    MaincontentComponent
+  ]
 })
 export class AppModule { }
