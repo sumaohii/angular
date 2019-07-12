28bd1613c02fbce6857c4184888ece8c270f2b72
JudyBui127 <ngan.bui@ryerson.ca>
2019-07-12 11:24:42 +0700

    fpstep2

---
+++ src/app/components/linkemail/linkemail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linkemail',
+  templateUrl: './linkemail.component.html',
+  styleUrls: ['./linkemail.component.css']
+})
+export class LinkemailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
