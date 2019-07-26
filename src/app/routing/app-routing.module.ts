bb1117fe1c5317b0a556ff8b956c7c950c65e6b4
judybui127 <ngan.bui@ryerson.ca>
2019-07-26 14:51:35 +0700

    redirect to admin

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -11,7 +11,8 @@ import { FooterComponent } from '../pages/footer/footer.component';
 import {KycComponent} from '../pages/kyc/kyc.component'
 import {DashboardComponent} from '../pages/dashboard/dashboard.component';
 import {LinkemailComponent} from '../components/linkemail/linkemail.component';
-import {KycImageComponent} from '../pages/kyc-image/kyc-image.component'
+import {KycImageComponent} from '../pages/kyc-image/kyc-image.component';
+import {VerifyKycComponent} from '../pages/admin-dashboard/verify-kyc/verify-kyc.component';
 const routes: Routes = [
   {
     path: 'register',
@@ -47,7 +48,12 @@ const routes: Routes = [
   },
 
   {path: 'dashboard',
-component: DashboardComponent},
+component: DashboardComponent
+  },
+  {
+    path: 'admin-dashboard/verify-kyc',
+    component: VerifyKycComponent
+  },
 
   { path: 'verify-account-email/:id',
     component: LinkemailComponent
