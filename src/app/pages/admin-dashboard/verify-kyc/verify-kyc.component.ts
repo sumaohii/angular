44f6eeb6fb2d42bd276fa0a85da35d0a33a5727b
judybui127 <ngan.bui@ryerson.ca>
2019-07-29 08:56:57 +0700

    admin-dashboard-kyc

--- src/app/pages/admin-dashboard/verify-kyc/verify-kyc.component.ts
+++ src/app/pages/admin-dashboard/verify-kyc/verify-kyc.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AdminService} from '../../../services/admin.service';
+import { all } from 'q';
 @Component({
   selector: 'app-verify-kyc',
   templateUrl: './verify-kyc.component.html',
   styleUrls: ['./verify-kyc.component.css']
 })
 export class VerifyKycComponent implements OnInit {
-
-  constructor() { }
+  allUser =[];
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+    this.adminService.fetchAllNonKyc().subscribe((res)=>{
+      console.log(res);
+    })
   }
 
 }
