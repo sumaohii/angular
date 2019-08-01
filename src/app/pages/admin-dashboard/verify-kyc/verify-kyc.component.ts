8a0b1523ddf2a7c5e42c60a3f74f25ab91ef2ba0
judybui127 <ngan.bui@ryerson.ca>
2019-08-01 15:26:15 +0700

    confirm/decline kyc

--- src/app/pages/admin-dashboard/verify-kyc/verify-kyc.component.ts
+++ src/app/pages/admin-dashboard/verify-kyc/verify-kyc.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import {AdminService} from '../../../services/admin.service';
 import {Router} from '@angular/router';
 import {UploadKycImg} from '../../../models/upload-kyc-img'
-
+import { AlertService} from '../../../components/alert/alert.service';
 @Component({
   selector: 'app-verify-kyc',
   templateUrl: './verify-kyc.component.html',
@@ -12,7 +12,7 @@ import {UploadKycImg} from '../../../models/upload-kyc-img'
 export class VerifyKycComponent implements OnInit {
   id: string;
   userImages: UploadKycImg[];
-  constructor(private adminService: AdminService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private adminService: AdminService, private router: Router, private route: ActivatedRoute, private alert: AlertService) { }
    
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('userId');
@@ -25,6 +25,22 @@ kycUser(){
   })
 }
 viewPhoto(){
+}
 
+confirm(){
+  this.adminService.confirmKyc(this.id).subscribe((res) => {
+    if (res.status==1)
+    this.alert.success(res.message+". Redirecting...");
+    else if(res.status==0)
+    this.alert.error(res.message+". Redirecting...");
+    setTimeout(() => {this.router.navigateByUrl('/admin-dashboard');}, 2000);
+})
+}
+decline(){
+  this.adminService.declineKyc(this.id).subscribe((res) => {
+    if (res.status==1)
+    this.alert.error(res.message);
+    setTimeout(() => {this.router.navigateByUrl('/admin-dashboard');}, 2000);
+})
 }
 }
